92979aeef3336804b1cd5559b63467a2a2792c2f
Matzilla <me@sigpwr.de>
2019-01-03 21:05:43 +0100

    Added setup.py with meta info

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    version='1.0alpha',
+    name='MLT',
+    authro='Matthias Meidinger',
+    packages=['MLT'],
+    license='Apache License 2.0',
+    url='https://github.com/Maddosaurus/MLT',
+)
