d00c94c51dbc2b4a4df320461213172637aec128
Matzilla <me@sigpwr.de>
2019-01-03 21:30:34 +0100

    add deps to setup.py

--- setup.py
+++ setup.py
@@ -7,4 +7,13 @@ setup(
     packages=['MLT'],
     license='Apache License 2.0',
     url='https://github.com/Maddosaurus/MLT',
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'xgboost',
+        'Keras',
+        'pyod',
+        'tensorflow',
+        'natsort'
+    ]
 )
