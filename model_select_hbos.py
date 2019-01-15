4c292dc9d407564e422c5dc1d475336fbb068142
Matthias <me@sigpwr.de>
2019-01-15 11:18:28 +0100

    Add CICIDS Randomized to Model Selection

--- model_select_hbos.py
+++ model_select_hbos.py
@@ -6,6 +6,8 @@ sys.path.insert(0, os.path.abspath('./MLT'))
 import MLT.run
 
 def main():
+    # NSL_KDD
+
     # Defaults
     parser = MLT.run.create_parser()
     args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl', '--hbos', '10', '0.1', '0.1'])
@@ -98,5 +100,99 @@ def main():
     MLT.run.main(args)
 
 
+    
+
+    # CIC6r
+
+    # Defaults
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.1'])
+    print(args)
+    MLT.run.main(args)
+
+    # n_bins
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '5', '0.1', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '20', '0.1', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '50', '0.1', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '100', '0.1', '0.1'])
+    MLT.run.main(args)
+
+
+    # alpha
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.2', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.3', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.4', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.5', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.6', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.7', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.8', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.9', '0.1'])
+    MLT.run.main(args)
+
+
+    # tol
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.2'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.3'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.4'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.5'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.6'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.7'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.8'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--cic6r', '--hbos', '10', '0.1', '0.9'])
+    MLT.run.main(args)
 if __name__ == '__main__':
     main()
