63c81ea5d6e19f558d20b0de96b664e477c9c593
Matthias <me@sigpwr.de>
2019-01-29 14:49:07 +0100

    MInimize training time by increasing batch size

--- model_select_autoenc.py
+++ model_select_autoenc.py
@@ -27,57 +27,57 @@ def main():
 
     # Epochs
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '10', '0.2', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '10', '0.2', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '20', '0.2', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '20', '0.2', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '50', '0.2', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '50', '0.2', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '150', '0.2', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '150', '0.2', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '200', '0.2', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '200', '0.2', '0.1'])
     MLT.run.main(args)
 
     # Dropout
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '100', '0.1', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '100', '0.1', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '100', '0.4', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '100', '0.4', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '100', '0.6', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '100', '0.6', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '100', '0.8', '0.1'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '100', '0.8', '0.1'])
     MLT.run.main(args)
 
     # Contamination
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '100', '0.2', '0.2'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '100', '0.2', '0.2'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '100', '0.2', '0.3'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '100', '0.2', '0.3'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '100', '0.2', '0.4'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '100', '0.2', '0.4'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '32', '100', '0.2', '0.5'])
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '128', '100', '0.2', '0.5'])
     MLT.run.main(args)
 
 
