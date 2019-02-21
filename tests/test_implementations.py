807fbf42b02876e475476e91c2cd1f3a2d11668d
Matthias <me@sigpwr.de>
2019-02-21 11:29:32 +0100

    Extend tests for implementations and run param parsing

--- tests/test_implementations.py
+++ tests/test_implementations.py
@@ -12,19 +12,41 @@ from .context import mlt_hbos, mlt_autoencoder, mlt_iforest, LSTM_2_Multiclass, 
 
 
 def test_hbos():
-    assert isinstance(mlt_hbos._create_model(), HBOS)
+    hbo = mlt_hbos._create_model(n_bins=42.0, alpha=0.4, tol=0.5)
+    assert isinstance(hbo, HBOS)
+    assert hbo.n_bins == 42
+    assert hbo.alpha == 0.4
+    assert hbo.tol == 0.5
 
 def test_autoencoder():
-    assert isinstance(mlt_autoencoder._create_model(hidden_neurons=[2,1,1,2]), AutoEncoder)
+    ae = mlt_autoencoder._create_model(epochs=2.0, batch_size=4.0, contamination=0.4, dropout_rate=0.11, hidden_neurons=[2,1,1,2])
+    assert isinstance(ae, AutoEncoder)
+    assert ae.epochs == 2
+    assert ae.batch_size == 4
+    assert ae.contamination == 0.4
+    assert ae.dropout_rate == 0.11
 
 def test_isolationforest():
-    assert isinstance(mlt_iforest._create_model(), IForest)
+    iforest= mlt_iforest._create_model(n_estimators=99.0, contamination=0.3, max_features=0.4, bootstrap=1.0)
+    assert isinstance(iforest, IForest)
+    assert iforest.n_estimators == 99
+    assert iforest.contamination == 0.3
+    assert iforest.max_features == 0.4
+    assert iforest.bootstrap == True
 
 def test_lstm():
-    assert isinstance(LSTM_2_Multiclass._create_model(), Sequential)
+    lstm = LSTM_2_Multiclass._create_model(learning_rate=0.001)
+    assert isinstance(lstm, Sequential)
 
 def test_random_forest():
-    assert isinstance(RandomForest._createModel(), RandomForestClassifier)
+    rf = RandomForest._createModel(n_estimators=33.0, max_depth=2.0)
+    assert isinstance(rf, RandomForestClassifier)
+    assert rf.n_estimators == 33
+    assert rf.max_depth == 2
 
 def test_xgboost():
-    assert isinstance(XGBoost._create_model(), XGBClassifier)
+    xgb = XGBoost._create_model(n_estimators=42.0, max_depth=1.0, learning_rate=0.3)
+    assert isinstance(xgb, XGBClassifier)
+    assert xgb.n_estimators == 42
+    assert xgb.max_depth == 1
+    assert xgb.learning_rate == 0.3
--- tests/test_run.py
+++ tests/test_run.py
@@ -2,7 +2,62 @@
 import pytest
 import argparse
 
-from .context import run
+from .context import run, base_runner
 
 def test_create_parser():
     assert isinstance(run.create_parser(), argparse.ArgumentParser)
+
+def test_nsl_kfold(monkeypatch):
+    def mock_run_nsl(args):
+        assert args.kfolds == 2
+        assert args.AutoEncoder[0] == 32.0
+        assert args.AutoEncoder[1] == 100.0
+        assert args.AutoEncoder[2] == 0.2
+        assert args.AutoEncoder[3] == 0.5
+        assert args.AutoEncoder[4] == 0.001
+
+    monkeypatch.setattr(base_runner, 'run_NSL', mock_run_nsl)
+    parser = run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '2', '--nsl16', '--AutoEncoder', '32', '100', '0.2', '0.5', '0.001'])
+    run.main(args)
+
+def test_nsl_single(monkeypatch):
+    def mock_run_nsl(args):
+        assert args.AutoEncoder[0] == 32.0
+        assert args.AutoEncoder[1] == 100.0
+        assert args.AutoEncoder[2] == 0.2
+        assert args.AutoEncoder[3] == 0.5
+        assert args.AutoEncoder[4] == 0.001
+
+    monkeypatch.setattr(base_runner, 'run_NSL', mock_run_nsl)
+    parser = run.create_parser()
+    args = parser.parse_args(['--unsupervised', '--single', '--nsl16', '--AutoEncoder', '32', '100', '0.2', '0.5', '0.001'])
+    run.main(args)
+
+
+def test_cic_kfold(monkeypatch):
+    def mock_run_cic(args):
+        assert args.kfolds == 2
+        assert args.AutoEncoder[0] == 32.0
+        assert args.AutoEncoder[1] == 100.0
+        assert args.AutoEncoder[2] == 0.2
+        assert args.AutoEncoder[3] == 0.5
+        assert args.AutoEncoder[4] == 0.001
+
+    monkeypatch.setattr(base_runner, 'run_CIC', mock_run_cic)
+    parser = run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '2', '--cic16', '--AutoEncoder', '32', '100', '0.2', '0.5', '0.001'])
+    run.main(args)
+
+def test_cic_single(monkeypatch):
+    def mock_run_cic(args):
+        assert args.AutoEncoder[0] == 32.0
+        assert args.AutoEncoder[1] == 100.0
+        assert args.AutoEncoder[2] == 0.2
+        assert args.AutoEncoder[3] == 0.5
+        assert args.AutoEncoder[4] == 0.001
+
+    monkeypatch.setattr(base_runner, 'run_CIC', mock_run_cic)
+    parser = run.create_parser()
+    args = parser.parse_args(['--unsupervised', '--single', '--cic16', '--AutoEncoder', '32', '100', '0.2', '0.5', '0.001'])
+    run.main(args)
