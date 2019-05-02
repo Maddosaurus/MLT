7c5d8244d46554b981f75067a5626a39c5420300
Matthias <me@sigpwr.de>
2019-05-02 10:31:44 +0200

    Update call params for XGB MS

--- model_select_xgb_cic.py
+++ model_select_xgb_cic.py
@@ -11,19 +11,19 @@ def main():
 
     # max_depth
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--CICt', '--XGBoost', '100', '3', '0.1'])
+    args = parser.parse_args(['-k', '10', '--CICt', '--XGBoost', '100', '3', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--CICt', '--XGBoost', '100', '10', '0.1'])
+    args = parser.parse_args(['-k', '10', '--CICt', '--XGBoost', '100', '10', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--CICt', '--XGBoost', '100', '50', '0.1'])
+    args = parser.parse_args(['-k', '10', '--CICt', '--XGBoost', '100', '50', '0.1'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--CICt', '--XGBoost', '100', '100', '0.1', '--mail'])
+    args = parser.parse_args(['-k', '10', '--CICt', '--XGBoost', '100', '100', '0.1', '--mail'])
     MLT.run.main(args)
 
 
