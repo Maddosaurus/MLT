9d6ff0114e8c9f9ab32c0ea40821f4b4c6ec64fe
Matthias <me@sigpwr.de>
2019-05-07 09:31:35 +0200

    Final step MS NSL AE

--- model_select_autoenc_nsl.py
+++ model_select_autoenc_nsl.py
@@ -44,39 +44,39 @@ def main():
     # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '200', '0.2', '0.001', '--mail'])
     # MLT.run.main(args)
 
-    # # Dropout
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.1', '0.001'])
-    MLT.run.main(args)
-
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.4', '0.001'])
-    MLT.run.main(args)
-
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.6', '0.001'])
-    MLT.run.main(args)
-
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.8', '0.001', '--mail'])
-    MLT.run.main(args)
-
-    # # Learning Rate
+    # # Dropout with Epochs=100, Epochs=512
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.2', '0.0001'])
+    # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.1', '0.001'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.2', '0.001'])
+    # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.4', '0.001'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.2', '0.01'])
+    # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.6', '0.001'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.2', '0.1', '--mail'])
+    # args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.8', '0.001', '--mail'])
     # MLT.run.main(args)
+
+    # # Learning Rate  with Epochs=100, Epochs=512, Dropout=0.2
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.2', '0.0001'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.2', '0.001'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.2', '0.01'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['--unsupervised', '-k', '10', '--nsl16', '--AutoEncoder', '512', '100', '0.2', '0.1', '--mail'])
+    MLT.run.main(args)
 
 if __name__ == '__main__':
     main()
