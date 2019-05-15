95d897a54dad858d784d03236ea4cb1ef06de226
Matthias <me@sigpwr.de>
2019-05-15 19:04:04 +0200

    Corrected call params

--- model_select_isolationforest_splunk.py
+++ model_select_isolationforest_splunk.py
@@ -10,87 +10,87 @@ def main():
 
     # Estimators
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '100', '1.0', 'True'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '100', '1.0', 'True'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '150', '1.0', 'True'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '150', '1.0', 'True'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '200', '1.0', 'True'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '200', '1.0', 'True'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '250', '1.0', 'True'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '250', '1.0', 'True'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '300', '1.0', 'True'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '300', '1.0', 'True'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '350', '1.0', 'True'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '350', '1.0', 'True'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '1.0', 'True'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '1.0', 'True'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '450', '1.0', 'True'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '450', '1.0', 'True'])
     MLT.run.main(args)
 
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '500', '1.0', 'True', '--mail'])
+    args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '500', '1.0', 'True', '--mail'])
     MLT.run.main(args)
 
 
     # max_features
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.1', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.1', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.2', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.2', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.3', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.3', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.4', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.4', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.5', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.5', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.6', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.6', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.7', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.7', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.8', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.8', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.9', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '0.9', 'True'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '1.0', 'True'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '1.0', 'True'])
     # MLT.run.main(args)
 
 
     # Bootstrap
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--anomaly' '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '1.0', 'False', '--mail'])
+    # args = parser.parse_args(['--anomaly', '--unsupervised', '-k', '10', '--splunkr', '--IsolationForest', '400', '1.0', 'False', '--mail'])
     # MLT.run.main(args)
 
 
