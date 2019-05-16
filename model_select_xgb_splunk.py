418421f71e6ce6cfa4b65e1c5795eece0a9ff372
Matthias <me@sigpwr.de>
2019-05-16 10:24:27 +0200

    XGB Spl MS Final run

--- model_select_xgb_splunk.py
+++ model_select_xgb_splunk.py
@@ -27,43 +27,43 @@ def main():
 
 
     # estimators with max_depth = 50
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '50', '50', '0.1'])
-    MLT.run.main(args)
+    # parser = MLT.run.create_parser()
+    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '50', '50', '0.1'])
+    # MLT.run.main(args)
     
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '100', '50', '0.1'])
-    MLT.run.main(args)
-
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '50', '0.1']) # <--- Winner!
-    MLT.run.main(args)
-
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '200', '50', '0.1'])
-    MLT.run.main(args)
-
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '500', '50', '0.1'])
-    MLT.run.main(args)
-
-
-    # learn rate with max_depth = 10 and n_estimators = 150 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '10', '0.1'])
+    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '100', '50', '0.1'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '10', '0.01'])
+    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '50', '0.1']) # <--- Winner!
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '10', '0.001'])
+    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '200', '50', '0.1'])
     # MLT.run.main(args)
 
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '10', '0.0001'])
+    # args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '500', '50', '0.1', '--mail'])
     # MLT.run.main(args)
+
+
+    # learn rate with max_depth = 50 and n_estimators = 150 
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '50', '0.1'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '50', '0.01'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '50', '0.001'])
+    MLT.run.main(args)
+
+    parser = MLT.run.create_parser()
+    args = parser.parse_args(['-k', '10', '--splunkr', '--XGBoost', '150', '50', '0.0001', '--mail'])
+    MLT.run.main(args)
 
 
 if __name__ == '__main__':
