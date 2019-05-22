916a446da4fcc84ca15d520bb60ef855edcd325d
Matthias <me@sigpwr.de>
2019-05-22 09:35:01 +0200

    FB run for HBOS CIC

--- final_benchmarks_cic.py
+++ final_benchmarks_cic.py
@@ -5,30 +5,30 @@ sys.path.insert(0, os.path.abspath('./MLT'))
 import MLT.run
 
 def main():
-    # Isolation Forest
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '--single', '--CICt', '--IsolationForest', '100', '0.5', 'True'])
-    MLT.run.main(args)
-
-    # HBOS
+    # # Isolation Forest
     # parser = MLT.run.create_parser()
-    # args = parser.parse_args(['--unsupervised', '--single', '--CICt', '--HBOS', '27', '0.6', '0.1'])
+    # args = parser.parse_args(['--unsupervised', '--single', '--CICt', '--IsolationForest', '100', '0.5', 'True'])
     # MLT.run.main(args)
 
-    # AutoEncoder
+    # HBOS
     parser = MLT.run.create_parser()
-    args = parser.parse_args(['--unsupervised', '--single', '--CICt', '--AutoEncoder', '4096', '100', '0.2', '0.001'])
+    args = parser.parse_args(['--unsupervised', '--single', '--CICt', '--HBOS', '5', '0.2', '0.1'])
     MLT.run.main(args)
 
-    # XGBOD
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['--single', '--CICt', '--XGBoost', '100', '10', '0.1'])
-    MLT.run.main(args)
+    # # AutoEncoder
+    # parser = MLT.run.create_parser()
+    # args = parser.parse_args(['--unsupervised', '--single', '--CICt', '--AutoEncoder', '4096', '100', '0.2', '0.001'])
+    # MLT.run.main(args)
 
-    #Random Forest
-    parser = MLT.run.create_parser()
-    args = parser.parse_args(['--single', '--CICt', '--RandomForest', '100', '0', '--mail'])
-    MLT.run.main(args)
+    # # XGBOD
+    # parser = MLT.run.create_parser()
+    # args = parser.parse_args(['--single', '--CICt', '--XGBoost', '100', '10', '0.1'])
+    # MLT.run.main(args)
+
+    # #Random Forest
+    # parser = MLT.run.create_parser()
+    # args = parser.parse_args(['--single', '--CICt', '--RandomForest', '100', '0', '--mail'])
+    # MLT.run.main(args)
 
 if __name__ == '__main__':
     main()
